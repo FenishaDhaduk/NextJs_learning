e2aa37d9d5d0da1c6bd352f8d4f63c0fe82820a6
FenishaDhaduk <fenishadhaduk@gmail.com>
2023-07-13 19:06:23 +0530

    home page

--- next.config.js
+++ next.config.js
@@ -5,3 +5,5 @@ const nextConfig = {
   },
 };
 module.exports = nextConfig;
+
+// git remote set-url origin https://FenishaDhaduk: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@github.com/FenishaDhaduk/NextJs_learning.git
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 // import React from 'react'
 
+import Link from "next/link";
+
 export default function index() {
-  return <div>home page</div>;
+  return (
+    <div>
+      <h1>Home page</h1>
+      <div>
+        <Link href="/Component/about">ABOUT PAGE</Link>
+      </div>
+    </div>
+  );
 }
