12050e2199ef6e09ad55fc4e0bf7f3d5285c958f
FenishaDhaduk <fenishadhaduk@gmail.com>
2023-07-14 16:30:09 +0530

    website

--- src/app/component/Herosection.js
+++ src/app/component/Herosection.js
@@ -16,9 +16,7 @@ export default function Herosection() {
         <div className={style.container}>
           <div className={style.grid_two_section}>
             <div className={herostyle.hero_content}>
-              <h1>
-                <b>LET'S WATCH MOVIE TOGETHER</b>
-              </h1>
+              <h1>LET&apos;S WATCH MOVIE TOGETHER</h1>
               <p>
                 &#128512; From award-winning dramas to blockbuster action
                 movies, we&apos;ve got you covered. Browse our selection of the
